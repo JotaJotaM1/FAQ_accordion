e975d51130d9b48e93cacfd9ddccc4f96b382ffe
JotaJotaM1 <johnjames_89@hotmail.com>
2024-08-15 16:16:32 -0500

    The script is adjusted so that clicking the title toggles the accordion

--- docs/script.js
+++ docs/script.js
@@ -1,4 +1,4 @@
-const accord = document.querySelectorAll(".accord")
+const accord = document.querySelectorAll(".accord");
 
 const checkSigns = (question, answer) => {
     if (answer.classList.contains("active")) {
@@ -6,19 +6,25 @@ const checkSigns = (question, answer) => {
     } else {
         question.children[1].src = "./images/icon-plus.svg";
     }
-
-}
+};
 
 accord.forEach((e) => {
     const question = e.querySelector(".question");
     const answer = e.querySelector(".answer");
+    const questionText = question.querySelector("p");
 
+    // Inicializa el ícono según el estado actual de la respuesta
     checkSigns(question, answer);
 
+    // Maneja el clic en el ícono
     question.children[1].addEventListener("click", () => {
         answer.classList.toggle("active");
-        checkSigns(question, answer)
-
+        checkSigns(question, answer);
     });
 
-})
+    // Maneja el clic en el <p> que contiene la pregunta
+    questionText.addEventListener("click", () => {
+        answer.classList.toggle("active");
+        checkSigns(question, answer);
+    });
+});
